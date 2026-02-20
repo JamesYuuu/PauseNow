981163855483be0572b458f58d22b2b4694fd877
JamesYuuu <yujunda20@mails.ucas.ac.cn>
2026-02-20 19:22:00 +0800

    Initial Commit

---
+++ PauseNow/PauseNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PauseNowApp.swift
+//  PauseNow
+//
+//  Created by JamesYu on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct PauseNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
